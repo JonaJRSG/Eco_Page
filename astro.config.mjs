ce925822843a1668dc808b4a1dd25647a23a99b9
JonaJRSG <jonathan.serna@hotmail.com>
2024-03-26 22:10:44 -0600

    Pirmer commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
