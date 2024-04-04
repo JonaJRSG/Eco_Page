6a14cb41b31980bb1d92e560c9a2da8a3378ac51
JonaJRSG <jonathan.serna@hotmail.com>
2024-04-04 14:01:04 -0600

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,21 @@ import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
 
+const site = "https://JonaJRSG.github.io";
+
 // https://astro.build/config
 export default defineConfig({
+  site: site,
+
   integrations: [tailwind(), solidJs()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
     fallback: {
-      en: "es"
+      en: "es",
     },
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      prefixDefaultLocale: false,
+    },
+  },
 });
