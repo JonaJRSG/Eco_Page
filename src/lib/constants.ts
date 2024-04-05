d97497d179bda54cbe8ee1035ca78348603de5d4
JonaJRSG <jonathan.serna@hotmail.com>
2024-04-04 20:22:06 -0600

    Cambios para meta

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,7 @@
+export const Des = {
+  main: "¡Bienvenido! Soy un estudiante de software apasionado por la informática, especializado en desarrollo web. Desde programación front-end hasta administración de bases de datos, mi experiencia abarca diversas áreas del desarrollo. Mis principales herramientas incluyen JavaScript, TypeScript, Node.js, Astro, React, Python y SQL. Este sitio web sirve como mi repositorio personal, blog y carta de presentación. ¡Explora mi trabajo y conoce más sobre mí aquí!",
+};
+
 export const LINKS = {
   github: "https://github.com/JonaJRSG",
   instagram: "https://www.instagram.com/jonathan.rsg.serna/",
